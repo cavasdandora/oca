41b2483fc6e5afa672c59b0462b29876700c7e25
Dora <dora.cavasdan@gmail.com>
2018-10-02 10:18:08 +0300

    test

---
+++ src/Zoo.java
@@ -0,0 +1,11 @@
+public class Zoo {
+    public static void main(String[] args) {
+       int n =10;
+       int y= n++ + --n - n++ + n-- + ++n - --n + n++ + ++n;
+
+        System.out.println("y" +y);
+       System.out.println("n" +n);
+Str
+
+    }
+}
