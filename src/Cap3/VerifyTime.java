83e4eaadcecbfd0eb02ec21a2a28f09a3fa0dba1
Dora <dora.cavasdan@gmail.com>
2018-10-09 17:45:45 +0300

    test

--- src/Cap3/VerifyTime.java
+++ src/Cap3/VerifyTime.java
@@ -12,27 +12,30 @@ public class VerifyTime {
 
     public void testLocalDateTime(LocalTime startTime, LocalTime endTime, LocalDateTime date) {
 
+        if (date.toLocalTime().isAfter(startTime) && (date.toLocalTime().isBefore(endTime))) {
 
+            System.out.print("your date is intradate");
+        } else {
+            System.out.print("your date is extradate");
+        }
 
     }
 
     public static void main(String args[]) {
         VerifyTime time1 = new VerifyTime();
 
-        LocalTime startTime = LocalTime.of( 10, 00, 00);
-        LocalTime endTime = LocalTime.of( 16, 00, 00);
-        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 10, 01, 00);
+        LocalTime startTime = LocalTime.of( 16, 00, 00);
+        LocalTime endTime = LocalTime.of( 10, 00, 00);
+        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 16, 01, 00);
 
-//        Period p = Period.of(1,1,1);
-//        date = date.minus(p);
-//        DateTimeFormatter f = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
-//        System.out.println(date.format(f));
-//
-//        String date2 = date.format(f);
-//        System.out.println(date2);
+        System.out.print(date.toLocalDate());
 
 
-       
+
+
+
+
+
         time1.testLocalDateTime(startTime, endTime, date);
 
 
