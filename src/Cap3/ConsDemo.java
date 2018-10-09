516301baed6476e712cb8c0a814c940e9432a1af
Dora <dora.cavasdan@gmail.com>
2018-10-09 22:35:24 +0300

    book example

---
+++ src/Cap3/ConsDemo.java
@@ -0,0 +1,8 @@
+package Cap3;
+public class ConsDemo {
+    public static void main(String args[]) {
+        MyClass t1 = new MyClass();
+        MyClass t2 = new MyClass();
+        System.out.println(t1 + " " + t2);
+    }
+}
---
+++ src/Cap3/MyClass.java
@@ -0,0 +1,5 @@
+package Cap3;
+
+class MyClass {
+
+}
