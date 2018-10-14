c981cd1da229ee6e6d420400b9f3426953e47321
Dora <dora.cavasdan@gmail.com>
2018-10-15 00:20:56 +0300

    book example

--- src/Cap3/VerifyTime.java
+++ src/Cap3/VerifyTime.java
@@ -9,57 +9,50 @@ public class VerifyTime {
     public boolean isIntraday(LocalTime startTime, LocalTime endTime, LocalDateTime date) {
 
 
-        LocalDate time = date.toLocalDate();
-
-        LocalTime mid = LocalTime.MIDNIGHT;
-
-        if ((startTime.getHour() < endTime.getHour()) &&
-                (date.toLocalTime().isAfter(startTime) && date.toLocalTime().isBefore(endTime))) {
+        if ((date.toLocalTime().isAfter(startTime) && date.toLocalTime().isBefore(endTime))) {
 
             return true;
-
-        } else if (startTime.getHour() > endTime.getHour()
-                && ((date.toLocalTime().isAfter(startTime) && (startTime.getHour() > mid.getHour())))) {
-
-            return true;
-        } else if (((startTime.getHour() > endTime.getHour())
-                && ((date.toLocalTime().isBefore(endTime)) && (mid.getHour() < endTime.getHour())))) {
-
-            return true;
-
-        } else if (((startTime.getHour() > endTime.getHour())
-
-                && date.toLocalTime().isAfter(endTime) && (mid.getHour() > endTime.getHour()))) {
-
+        }else if((date.toLocalTime().isBefore(startTime) && date.toLocalTime().isBefore(endTime))){
             return false;
-
-        } else {
-
-            return false;
-
         }
-    }
+        else if((date.toLocalTime().isAfter(startTime) && date.toLocalTime().isAfter(endTime))){
+            return false;
+        }
 
+        else if ((date.isBefore(LocalDateTime.MAX) && (date.toLocalTime().isAfter(startTime)))) {
+            return true;
+
+
+        } else if ((date.isAfter(LocalDateTime.MIN)) && ((date.toLocalTime().isBefore(endTime)))) {
+            return true;
+        } else if ((date.isAfter(LocalDateTime.MIN)) && date.toLocalTime().isAfter(endTime)) {
+            return false;
+        }
+
+
+        return false;
+    }
 
     public static void main(String args[]) {
         VerifyTime time1 = new VerifyTime();
 
         LocalTime startTime = LocalTime.of(10, 00, 00);
         LocalTime endTime = LocalTime.of(16, 00, 00);
-        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 9, 01, 00);
+        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 10, 01, 00);
+//
+//
+//        assert time1.isIntraday(startTime, endTime, date) == true;
+//
+//        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 9, 59, 00);
+//
+//        assert time1.isIntraday(startTime, endTime, date) == false;
+//        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 15, 59, 00);
+//
+//        assert time1.isIntraday(startTime, endTime, date) == true;
+//        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 16, 01, 00);
+//
+//        assert time1.isIntraday(startTime, endTime, date) == false;
 
-
-        assert time1.isIntraday(startTime, endTime, date) == false;
-
-        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 11, 01, 00);
-
-        assert time1.isIntraday(startTime, endTime, date) == true;
-
-        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 17, 01, 00);
-
-        assert time1.isIntraday(startTime, endTime, date) == false;
-
-        //------------
 
         startTime = LocalTime.of(18, 00, 00);
         endTime = LocalTime.of(3, 00, 00);
@@ -70,7 +63,7 @@ public class VerifyTime {
 
         date = LocalDateTime.of(2018, Month.OCTOBER, 1, 19, 01, 00);
 
-        assert time1.isIntraday(startTime, endTime, date) == true;
+        assert time1.isIntraday(startTime, endTime, date) == false;
 
         date = LocalDateTime.of(2018, Month.OCTOBER, 1, 2, 01, 00);
 
