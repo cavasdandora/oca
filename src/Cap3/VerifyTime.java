ad4d75141a341beba8e19dff9be7bb589ce7e183
Dora <dora.cavasdan@gmail.com>
2018-10-15 00:48:11 +0300

    book example

--- src/Cap3/VerifyTime.java
+++ src/Cap3/VerifyTime.java
@@ -12,22 +12,32 @@ public class VerifyTime {
         if ((date.toLocalTime().isAfter(startTime) && date.toLocalTime().isBefore(endTime))) {
 
             return true;
-        }else if((date.toLocalTime().isBefore(startTime) && date.toLocalTime().isBefore(endTime))){
+
+        }
+       else if ((date.toLocalTime().isBefore(startTime) && date.toLocalTime().isBefore(endTime))) {
+
+            return true;
+        }
+        else if ((date.toLocalTime().isAfter(startTime) && date.toLocalTime().isAfter(endTime))) {
+
+            return true;
+        }
+        else if((date.toLocalTime().isBefore(startTime) && date.toLocalTime().isBefore(endTime))){
             return false;
         }
         else if((date.toLocalTime().isAfter(startTime) && date.toLocalTime().isAfter(endTime))){
             return false;
         }
-
-        else if ((date.isBefore(LocalDateTime.MAX) && (date.toLocalTime().isAfter(startTime)))) {
-            return true;
-
-
-        } else if ((date.isAfter(LocalDateTime.MIN)) && ((date.toLocalTime().isBefore(endTime)))) {
-            return true;
-        } else if ((date.isAfter(LocalDateTime.MIN)) && date.toLocalTime().isAfter(endTime)) {
-            return false;
-        }
+//
+//        else if ((date.isBefore(LocalDateTime.MAX) && (date.toLocalTime().isAfter(startTime)))) {
+//            return true;
+//
+//
+//        } else if ((date.isAfter(LocalDateTime.MIN)) && ((date.toLocalTime().isBefore(endTime)))) {
+//            return true;
+//        } else if ((date.isAfter(LocalDateTime.MIN)) && date.toLocalTime().isAfter(endTime)) {
+//            return false;
+//        }
 
 
         return false;
@@ -36,40 +46,10 @@ public class VerifyTime {
     public static void main(String args[]) {
         VerifyTime time1 = new VerifyTime();
 
-        LocalTime startTime = LocalTime.of(10, 00, 00);
-        LocalTime endTime = LocalTime.of(16, 00, 00);
+        LocalTime startTime = LocalTime.of(16, 00, 00);
+        LocalTime endTime = LocalTime.of(10, 00, 00);
         LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 10, 01, 00);
-//
-//
-//        assert time1.isIntraday(startTime, endTime, date) == true;
-//
-//        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 9, 59, 00);
-//
-//        assert time1.isIntraday(startTime, endTime, date) == false;
-//        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 15, 59, 00);
-//
-//        assert time1.isIntraday(startTime, endTime, date) == true;
-//        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 16, 01, 00);
-//
-//        assert time1.isIntraday(startTime, endTime, date) == false;
 
-
-        startTime = LocalTime.of(18, 00, 00);
-        endTime = LocalTime.of(3, 00, 00);
-        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 17, 01, 00);
-
-
-        assert time1.isIntraday(startTime, endTime, date) == false;
-
-        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 19, 01, 00);
-
-        assert time1.isIntraday(startTime, endTime, date) == false;
-
-        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 2, 01, 00);
-
-        assert time1.isIntraday(startTime, endTime, date) == true;
-
-        date = LocalDateTime.of(2018, Month.OCTOBER, 1, 4, 01, 00);
 
         assert time1.isIntraday(startTime, endTime, date) == false;
 
--- src/Cap3/VerifyTimeTest.java
+++ src/Cap3/VerifyTimeTest.java
@@ -12,10 +12,9 @@ class VerifyTimeTest {
 
     @Test
     void isIntraday_SOD_SameDay_EOD_InPast_BeforeSOD() {
-        LocalTime startTime = LocalTime.of(10, 00, 00);
-        LocalTime endTime = LocalTime.of(16, 00, 00);
-        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 9, 59, 59);
-
+        LocalTime startTime = LocalTime.of(16, 00, 00);
+        LocalTime endTime = LocalTime.of(10, 00, 00);
+        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 10, 01, 00);
 
         VerifyTime time1 = new VerifyTime();
         Assertions.assertEquals(false, time1.isIntraday(startTime, endTime, date));
@@ -23,12 +22,70 @@ class VerifyTimeTest {
 
     @Test
     void isIntraday_SOD_SameDay_EOD_InPast_BeforeSOD1() {
-        LocalTime startTime = LocalTime.of(10, 00, 00);
-        LocalTime endTime = LocalTime.of(16, 00, 00);
-        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 9, 59, 59);
+        LocalTime startTime = LocalTime.of(16, 00, 00);
+        LocalTime endTime = LocalTime.of(10, 00, 00);
+        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 9, 59, 00);
 
 
         VerifyTime time1 = new VerifyTime();
         Assertions.assertEquals(true, time1.isIntraday(startTime, endTime, date));
     }
-}
+    @Test
+    void isIntraday_SOD_SameDay_EOD_InPast_BeforeSOD2 () {
+        LocalTime startTime = LocalTime.of(16, 00, 00);
+        LocalTime endTime = LocalTime.of(10, 00, 00);
+        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 15, 59, 00);
+
+
+        VerifyTime time1 = new VerifyTime();
+        Assertions.assertEquals(false, time1.isIntraday(startTime, endTime, date));
+    }
+    @Test
+     void isIntraday_SOD_SameDay_EOD_InPast_BeforeSOD3 () {
+                LocalTime startTime = LocalTime.of(16, 00, 00);
+                LocalTime endTime = LocalTime.of(10, 00, 00);
+                LocalDateTime  date = LocalDateTime.of(2018, Month.OCTOBER, 1, 16, 01, 00);
+
+                VerifyTime time1 = new VerifyTime();
+                Assertions.assertEquals(true, time1.isIntraday(startTime, endTime, date));
+    }
+    @Test
+    void isIntraday_SOD_SameDay_EOD_InPast_BeforeSOD35 () {
+        LocalTime   startTime = LocalTime.of(18, 00, 00);
+        LocalTime endTime = LocalTime.of(3, 00, 00);
+        LocalDateTime  date = LocalDateTime.of(2018, Month.OCTOBER, 1, 17, 01, 00);
+
+        VerifyTime time1 = new VerifyTime();
+        Assertions.assertEquals(false, time1.isIntraday(startTime, endTime, date));
+    }
+    @Test
+    void isIntraday_SOD_SameDay_EOD_InPast_BeforeSOD33 () {
+        LocalTime   startTime = LocalTime.of(18, 00, 00);
+        LocalTime endTime = LocalTime.of(3, 00, 00);
+        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 19, 01, 00);
+
+        VerifyTime time1 = new VerifyTime();
+        Assertions.assertEquals(true, time1.isIntraday(startTime, endTime, date));
+    }
+    @Test
+    void isIntraday_SOD_SameDay_EOD_InPast_BeforeSOD37 () {
+        LocalTime   startTime = LocalTime.of(18, 00, 00);
+        LocalTime endTime = LocalTime.of(3, 00, 00);
+        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 2, 01, 00);
+
+        VerifyTime time1 = new VerifyTime();
+        Assertions.assertEquals(true, time1.isIntraday(startTime, endTime, date));
+    }
+    @Test
+    void isIntraday_SOD_SameDay_EOD_InPast_BeforeSOD4 () {
+        LocalTime   startTime = LocalTime.of(18, 00, 00);
+        LocalTime endTime = LocalTime.of(3, 00, 00);
+        LocalDateTime date = LocalDateTime.of(2018, Month.OCTOBER, 1, 4, 01, 00);
+
+        VerifyTime time1 = new VerifyTime();
+        Assertions.assertEquals(false, time1.isIntraday(startTime, endTime, date));
+    }
+
+
+
+        }
